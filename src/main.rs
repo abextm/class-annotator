a53979624aab559665e5d8d613427f5a73a7df64
Max Weber <mii7303@gmail.com>
2021-08-26 05:00:04 -0600

    bytecode reading

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use std::io::Read;
 
 #[macro_use]
 extern crate error_chain;
-#[macro_use]
-extern crate enum_kinds;
 
 mod errors {
 	error_chain! {
@@ -29,7 +27,7 @@ fn main() {
 struct RememberingBuffer<T: io::Read> {
 	pub reader: T,
 	buf: Vec<u8>,
-	offset: usize,
+	pub offset: usize,
 	column_width: usize,
 	indent: i32
 }
@@ -137,8 +135,7 @@ impl std::ops::IndexMut<CPIndex> for Vec<CPEntry> {
 	}
 }
 
-#[derive(Clone, Debug, EnumKind)]
-#[enum_kind(CPEntryKind)]
+#[derive(Clone, Debug)]
 enum CPEntry {
 	Unset,
 	Class(CPIndex),
@@ -154,18 +151,82 @@ enum CPEntry {
 	Utf8(String),
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum CPEntryKind {
+	AnyMethod,
+	AnyConstant1,
+	AnyConstant2,
+	Unset,
+	Class,
+	Field,
+	Method,
+	InterfaceMethod,
+	String,
+	Integer,
+	Long,
+	Float,
+	Double,
+	NameAndType,
+	Utf8,
+}
+
+impl From<&CPEntry> for CPEntryKind {
+	fn from(e: &CPEntry) -> Self {
+		match e {
+			&CPEntry::Unset => CPEntryKind::Unset,
+			&CPEntry::Class(_) => CPEntryKind::Class,
+			&CPEntry::Field(_, _)=> CPEntryKind::Field,
+			&CPEntry::Method(_, _)=> CPEntryKind::Method,
+			&CPEntry::InterfaceMethod(_, _)=> CPEntryKind::InterfaceMethod,
+			&CPEntry::String(_) => CPEntryKind::String,
+			&CPEntry::Integer(_)=> CPEntryKind::Integer,
+			&CPEntry::Long(_)=> CPEntryKind::Long,
+			&CPEntry::Float(_) => CPEntryKind::Float,
+			&CPEntry::Double(_) => CPEntryKind::Double,
+			&CPEntry::NameAndType(_, _) => CPEntryKind::NameAndType,
+			&CPEntry::Utf8(_) => CPEntryKind::Utf8,
+		}
+	}
+}
+
+impl CPEntryKind {
+	fn is(self, other: CPEntryKind) -> bool {
+		match self {
+			s if s == other => true,
+			CPEntryKind::AnyMethod => other == CPEntryKind::Method || other == CPEntryKind::InterfaceMethod,
+			CPEntryKind::AnyConstant1 => [
+				CPEntryKind::Integer,
+				CPEntryKind::Float,
+				CPEntryKind::Class,
+				CPEntryKind::String,
+				//CPEntryKind::MethodHandle,
+				//CPEntryKind::MethodType,
+				//CPEntryKind::Dynamic,
+			].contains(&other),
+			CPEntryKind::AnyConstant2 => [
+				CPEntryKind::Long,
+				CPEntryKind::Double,
+				//CPEntryKind::Dynamic,
+			].contains(&other),
+			_ => false,
+		}
+	}
+}
+
 struct CPLookup<'a>(&'a Vec<CPEntry>, CPEntryKind, CPIndex);
 impl std::fmt::Display for CPLookup<'_> {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{:?} at {} containing ", self.1, self.2)?;
 		let entry = &self.0[self.2];
 		if let &CPEntry::Unset = entry {
+			write!(f, "{:?} at {} containing ", self.1, self.2)?;
 			return Ok(());
 		}
 		let entry_type = CPEntryKind::from(entry);
-		if entry_type != self.1 {
+		if !self.1.is(entry_type) {
+			write!(f, "{:?} at {} containing ", self.1, self.2)?;
 			write!(f, "incorrect type {:?} ", entry_type)
 		} else {
+			write!(f, "{:?} at {} containing ", entry_type, self.2)?;
 			use CPEntry::*;
 			match entry {
 				&Unset => unreachable!(),
@@ -240,6 +301,12 @@ impl<T: io::Read> ClassReader<T> {
 	fn read_u16(&mut self) -> io::Result<u16> {
 		self.fi.read_u16::<BE>()
 	}
+	fn read_i16(&mut self) -> io::Result<i16> {
+		self.fi.read_i16::<BE>()
+	}
+	fn read_cp8(&mut self) -> io::Result<CPIndex> {
+		Ok(CPIndex(self.fi.read_u8()? as u16))
+	}
 	fn read_cp(&mut self) -> io::Result<CPIndex> {
 		Ok(CPIndex(self.fi.read_u16::<BE>()?))
 	}
@@ -248,6 +315,9 @@ impl<T: io::Read> ClassReader<T> {
 	}
 	fn read_u32(&mut self) -> io::Result<u32> {
 		self.fi.read_u32::<BE>()
+	}
+	fn read_i32(&mut self) -> io::Result<i32> {
+		self.fi.read_i32::<BE>()
 	}
 
 	fn read_magic(&mut self) -> Result<()> {
@@ -308,7 +378,7 @@ impl<T: io::Read> ClassReader<T> {
 					self.cp[cp_index] = CPEntry::String(string_index);
 				},
 				3 => {
-					let val = self.read_i32::<BE>()?;
+					let val = self.read_i32()?;
 					self.explain_read(format!("\
 {:>}: CONSTANT_Integer_info (JVMS§4.4.4)
 	32 bit integer value {}", cp_index, val));
@@ -618,10 +688,8 @@ implements {}", CPLookup(&self.cp, CPEntryKind::Class, iface)));
 		{
 			let code_length = self.read_u32()?;
 			self.explain_read(format!("method bytecode length {}", code_length));
-			let mut buf = vec![0; code_length as usize];
-			self.read_exact(&mut buf)?;
 			self.indent(1);
-			self.explain_read("method bytecode");
+			self.read_bytecode(self.offset + code_length as usize)?;
 			self.indent(-1);
 		}
 		{
@@ -648,6 +716,372 @@ exception handler {} from {} to {}. handler at {}; catches {}",
 			}
 		}
 		self.indent(-2);
+		Ok(())
+	}
+
+	fn read_maybewide(&mut self, is_wide: bool) -> Result<u16> {
+		if is_wide {
+			Ok(self.read_u16()?)
+		} else {
+			Ok(self.read_u8()? as u16)
+		}
+	}
+
+	fn explain_branch(&mut self, name: &'static str) -> Result<()> {
+		let offset = self.read_i16()? as i32;
+		let ins_offset =  self.offset as i32;
+		self.explain_read(format!("{} 0x{:>} (JVMS§6.5)", name, ins_offset + offset));
+		Ok(())
+	}
+
+	fn read_bytecode(&mut self, end: usize) -> Result<()> {
+		let mut buf = vec![0; end - self.offset];
+		let start = self.offset;
+		let mut is_wide = false;
+		while self.offset < end {
+			match self.read_u8()? {
+				0x32 => self.explain_read("aaload (JVMS§6.5)"),
+				0x53 => self.explain_read("aastore (JVMS§6.5)"),
+				0x01 => self.explain_read("aconst_null (JVMS§6.5)"),
+				0x25 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("aload {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x2a && index <= 0x2d => self.explain_read(format!("aload_{} (JVMS§6.5)", index - 0x2a)),
+				0xbd => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("anewarray {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Class, typ)));
+				},
+				0xb0 => self.explain_read("areturn (JVMS§6.5)"),
+				0xbe => self.explain_read("arraylength (JVMS§6.5)"),
+				0x3a => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("areturn {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x4b && index <= 0x4e => self.explain_read(format!("astore_{} (JVMS§6.5)", index - 0x4b)),
+				0xbf => self.explain_read("athrow (JVMS§6.5)"),
+				0x33 => self.explain_read("baload (JVMS§6.5)"),
+				0x54 => self.explain_read("bastore (JVMS§6.5)"),
+				0x10 => {
+					let value = self.read_i8()? as i32;
+					self.explain_read(format!("bipush {} (JVMS§6.5)", value))
+				},
+				0x34 => self.explain_read("caload (JVMS§6.5)"),
+				0x55 => self.explain_read("castore (JVMS§6.5)"),
+				0xc0 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("checkcast {} (JVMS§6.5)", index));
+				},
+				0x90 => self.explain_read("d2f (JVMS§6.5)"),
+				0x8e => self.explain_read("d2i (JVMS§6.5)"),
+				0x8f => self.explain_read("d2l (JVMS§6.5)"),
+				0x63 => self.explain_read("dadd (JVMS§6.5)"),
+				0x31 => self.explain_read("daload (JVMS§6.5)"),
+				0x52 => self.explain_read("dastore (JVMS§6.5)"),
+				0x98 => self.explain_read("dcmpg (JVMS§6.5)"),
+				0x97 => self.explain_read("dcmpl (JVMS§6.5)"),
+				0x0e => self.explain_read("dconst_0 (JVMS§6.5)"),
+				0x0f => self.explain_read("dconst_1 (JVMS§6.5)"),
+				0x6f => self.explain_read("ddiv (JVMS§6.5)"),
+				0x18 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("dload {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x26 && index <= 0x29 => self.explain_read(format!("dload_{} (JVMS§6.5)", index - 0x26)),
+				0x6b => self.explain_read("dmul (JVMS§6.5)"),
+				0x77 => self.explain_read("dneg (JVMS§6.5)"),
+				0x73 => self.explain_read("drem (JVMS§6.5)"),
+				0xaf => self.explain_read("dreturn (JVMS§6.5)"),
+				0x39 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("dstore {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x47 && index <= 0x4a => self.explain_read(format!("dstore_{} (JVMS§6.5)", index - 0x47)),
+				0x67 => self.explain_read("dsub (JVMS§6.5)"),
+				0x59 => self.explain_read("dup (JVMS§6.5)"),
+				0x5a => self.explain_read("dup_x1 (JVMS§6.5)"),
+				0x5b => self.explain_read("dup_x2 (JVMS§6.5)"),
+				0x5c => self.explain_read("dup2 (JVMS§6.5)"),
+				0x5d => self.explain_read("dup2_x1 (JVMS§6.5)"),
+				0x5e => self.explain_read("dup2_x2 (JVMS§6.5)"),
+				0x8d => self.explain_read("f2d (JVMS§6.5)"),
+				0x8b => self.explain_read("f2i (JVMS§6.5)"),
+				0x8c => self.explain_read("f2l (JVMS§6.5)"),
+				0x62 => self.explain_read("fadd (JVMS§6.5)"),
+				0x30 => self.explain_read("faload (JVMS§6.5)"),
+				0x51 => self.explain_read("fastore (JVMS§6.5)"),
+				0x96 => self.explain_read("fcmpg (JVMS§6.5)"),
+				0x95 => self.explain_read("fcmpl (JVMS§6.5)"),
+				v if v >= 0x0b && v <= 0x0d => self.explain_read(format!("fconst_{} (JVMS§6.5)", v - 0x0b)),
+				0x6e => self.explain_read("fdiv (JVMS§6.5)"),
+				0x17 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("fload {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x22 && index <= 0x25 => self.explain_read(format!("fload_{} (JVMS§6.5)", index - 0x22)),
+				0x6a => self.explain_read("fmul (JVMS§6.5)"),
+				0x76 => self.explain_read("fneg (JVMS§6.5)"),
+				0x72 => self.explain_read("frem (JVMS§6.5)"),
+				0xae => self.explain_read("freturn (JVMS§6.5)"),
+				0x38 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("fstore {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x43 && index <= 0x46 => self.explain_read(format!("fload_{} (JVMS§6.5)", index - 0x43)),
+				0x66 => self.explain_read("fsub (JVMS§6.5)"),
+				0xb4 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("getfield {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Field, typ)));
+				},
+				0xb2 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("getstatic {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Field, typ)));
+				},
+				0xa7 => self.explain_branch("goto")?,
+				0xc8 => {
+					let offset = self.read_i32()?;
+					self.fi.explain_read(format!("goto_w 0x{:>} (JVMS§6.5)", self.offset as i32 + offset))
+				},
+				0x91 => self.explain_read("i2b (JVMS§6.5)"),
+				0x92 => self.explain_read("i2c (JVMS§6.5)"),
+				0x87 => self.explain_read("i2d (JVMS§6.5)"),
+				0x86 => self.explain_read("i2f (JVMS§6.5)"),
+				0x85 => self.explain_read("i2l (JVMS§6.5)"),
+				0x93 => self.explain_read("i2s (JVMS§6.5)"),
+				0x60 => self.explain_read("iadd (JVMS§6.5)"),
+				0x2e => self.explain_read("iaload (JVMS§6.5)"),
+				0x7e => self.explain_read("iand (JVMS§6.5)"),
+				0x4f => self.explain_read("iastore (JVMS§6.5)"),
+				0x02 => self.explain_read("iconst_m1 (JVMS§6.5)"),
+				v if v >= 0x03 && v <= 0x08 => self.explain_read(format!("iconst_{} (JVMS§6.5)", v - 0x03)),
+				0x6c => self.explain_read("idiv (JVMS§6.5)"),
+				0xa5 => self.explain_branch("if_acmpeq")?,
+				0xa6 => self.explain_branch("if_acmpne")?,
+				0x9f => self.explain_branch("if_icmpeq")?,
+				0xa0 => self.explain_branch("if_icmpne")?,
+				0xa1 => self.explain_branch("if_icmplt")?,
+				0xa2 => self.explain_branch("if_icmpge")?,
+				0xa3 => self.explain_branch("if_icmpgt")?,
+				0xa4 => self.explain_branch("if_icmple")?,
+				0x99 => self.explain_branch("ifeq")?,
+				0x9a => self.explain_branch("ifne")?,
+				0x9b => self.explain_branch("iflt")?,
+				0x9c => self.explain_branch("ifge")?,
+				0x9d => self.explain_branch("ifgt")?,
+				0x9e => self.explain_branch("ifle")?,
+				0xc7 => self.explain_branch("ifnonnull")?,
+				0xc6 => self.explain_branch("ifnull")?,
+				0x84 => {
+					let index = self.read_maybewide(is_wide)?;
+					let value = if is_wide { self.read_i16()? } else { self.read_i8()? as i16 };
+					self.explain_read(format!("iinc {} += {} (JVMS§6.5)", index, value));
+				},
+				0x15 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("iload {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x1a && index <= 0x1d => self.explain_read(format!("iload_{} (JVMS§6.5)", index - 0x1a)),
+				0x68 => self.explain_read("imul (JVMS§6.5)"),
+				0x74 => self.explain_read("ineg (JVMS§6.5)"),
+				0xc1 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("instanceof {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Class, typ)));
+				},
+				/*0xba => {
+					let typ = self.read_cp()?;
+					let zeros = self.read_u16()?;
+					if zeros != 0 {
+						panic!("invalid invokedynamic");
+					}
+					self.explain_read(format!("invokedynamic {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::InvokeDynamicInfo, typ)));
+				},*/
+				0xb9 => {
+					let typ = self.read_cp()?;
+					let _arg_count = self.read_u8()?; // probably should check this idc
+					let zero = self.read_u8()?;
+					if zero != 0 {
+						panic!("invalid invokeinterface");
+					}
+					self.fi.explain_read(format!("invokeinterface {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::InterfaceMethod, typ)));
+				},
+				0xb7 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("invokespecial {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::AnyMethod, typ)));
+				},
+				0xb8 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("invokestatic {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::AnyMethod, typ)));
+				},
+				0xb6 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("invokevirtual {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Method, typ)));
+				},
+				0x80 => self.explain_read("ior (JVMS§6.5)"),
+				0x70 => self.explain_read("irem (JVMS§6.5)"),
+				0xac => self.explain_read("ireturn (JVMS§6.5)"),
+				0x78 => self.explain_read("ishl (JVMS§6.5)"),
+				0x7a => self.explain_read("ishr (JVMS§6.5)"),
+				0x36 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("istore {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x3b && index <= 0x3e => self.explain_read(format!("istore_{} (JVMS§6.5)", index - 0x3b)),
+				0x64 => self.explain_read("isub (JVMS§6.5)"),
+				0x7c => self.explain_read("iushr (JVMS§6.5)"),
+				0x82 => self.explain_read("ixor (JVMS§6.5)"),
+				0xa8 => self.explain_branch("jsr")?,
+				0xc9 => {
+					let offset = self.read_i32()?;
+					self.fi.explain_read(format!("jsr_w 0x{:>} (JVMS§6.5)", self.offset as i32 + offset))
+				},
+				0x8a => self.explain_read("l2d (JVMS§6.5)"),
+				0x89 => self.explain_read("l2f (JVMS§6.5)"),
+				0x88 => self.explain_read("l2i (JVMS§6.5)"),
+				0x61 => self.explain_read("ladd (JVMS§6.5)"),
+				0x2f => self.explain_read("laload (JVMS§6.5)"),
+				0x7f => self.explain_read("land (JVMS§6.5)"),
+				0x50 => self.explain_read("lastore (JVMS§6.5)"),
+				0x94 => self.explain_read("lcmp (JVMS§6.5)"),
+				0x09 => self.explain_read("lconst_0 (JVMS§6.5)"),
+				0x0a => self.explain_read("lconst_1 (JVMS§6.5)"),
+				0x12 => {
+					let typ = self.read_cp8()?;
+					self.fi.explain_read(format!("ldc {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::AnyConstant1, typ)));
+				},
+				0x13 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("ldc_w {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::AnyConstant1, typ)));
+				},
+				0x14 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("ldc2_w {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::AnyConstant2, typ)));
+				},
+				0x6d => self.explain_read("ldiv (JVMS§6.5)"),
+				0x16 => self.explain_read("lload (JVMS§6.5)"),
+				index if index >= 0x1e && index <= 0x21 => self.explain_read(format!("lload_{} (JVMS§6.5)", index - 0x1e)),
+				0x69 => self.explain_read("lmul (JVMS§6.5)"),
+				0x75 => self.explain_read("lneg (JVMS§6.5)"),
+				0xab => {
+					let bc_offset = self.offset as i32;
+					self.explain_read("lookupswitch (JVMS§6.5)");
+					self.indent(1);
+					let padding = (self.offset - start) % 4;
+					if padding > 0 {
+						self.read_exact(&mut vec![0; 4 - padding])?;
+						self.explain_read("padding")
+					}
+					let default = bc_offset + self.read_i32()?;
+					self.explain_read(format!("default branch {:>}", default));
+					let npairs = self.read_i32()?;
+					self.explain_read("number of cases");
+					self.indent(1);
+					for _ in 0..npairs {
+						let key = self.read_i32()?;
+						let branch = bc_offset + self.read_i32()?;
+						self.explain_read(format!("case {}: branch {:>}", key, branch));
+					}
+					self.indent(-2);
+				},
+				0x81 => self.explain_read("lor (JVMS§6.5)"),
+				0x71 => self.explain_read("lrem (JVMS§6.5)"),
+				0xad => self.explain_read("lreturn (JVMS§6.5)"),
+				0x79 => self.explain_read("lshl (JVMS§6.5)"),
+				0x7b => self.explain_read("lshr (JVMS§6.5)"),
+				0x37 => {
+					let index = self.read_maybewide(is_wide)?;
+					self.explain_read(format!("lstore {} (JVMS§6.5)", index));
+				},
+				index if index >= 0x3f && index <= 0x42 => self.explain_read(format!("lstore_{} (JVMS§6.5)", index - 0x3f)),
+				0x65 => self.explain_read("lsub (JVMS§6.5)"),
+				0x7d => self.explain_read("lushr (JVMS§6.5)"),
+				0x83 => self.explain_read("lxor (JVMS§6.5)"),
+				0xc2 => self.explain_read("monitorenter (JVMS§6.5)"),
+				0xc3 => self.explain_read("monitorexit (JVMS§6.5)"),
+				0xc5 => {
+					let typ = self.read_cp()?;
+					let dims = self.read_u8()?;
+					self.fi.explain_read(format!("multianewarray {} dimensions {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Class, typ), dims));
+				},
+				0xbb => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("new {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Class, typ)));
+				},
+				0xbc => {
+					let typ = self.read_u8()?;
+					let type_str = match typ {
+						4 => "boolean",
+						5 => "char",
+						6 => "float",
+						7 => "double",
+						8 => "byte",
+						9 => "short",
+						10 => "int",
+						11 => "long",
+						other => panic!("invalid newarray type {}", other),
+					};
+					self.explain_read(format!("newarray {} (JVMS§6.5)", type_str));
+				}
+				0x00 => self.explain_read("nop (JVMS§6.5)"),
+				0x57 => self.explain_read("pop (JVMS§6.5)"),
+				0x58 => self.explain_read("pop2 (JVMS§6.5)"),
+				0xb5 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("putfield {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Field, typ)));
+				},
+				0xb3 => {
+					let typ = self.read_cp()?;
+					self.fi.explain_read(format!("putstatic {} (JVMS§6.5)", CPLookup(&self.cp, CPEntryKind::Field, typ)));
+				},
+				0xa9 => self.explain_read("ret (JVMS§6.5)"),
+				0xb1 => self.explain_read("return (JVMS§6.5)"),
+				0x35 => self.explain_read("saload (JVMS§6.5)"),
+				0x56 => self.explain_read("sastore (JVMS§6.5)"),
+				0x11 => {
+					let value = self.read_i16()? as i32;
+					self.explain_read(format!("sipush {} (JVMS§6.5)", value))
+				},
+				0x5f => self.explain_read("swap (JVMS§6.5)"),
+				0xaa => {
+					let bc_offset = self.offset as i32;
+					self.explain_read("tableswitch (JVMS§6.5)");
+					self.indent(1);
+					let padding = (self.offset - start) % 4;
+					if padding > 0 {
+						self.read_exact(&mut vec![0; 4 - padding])?;
+						self.explain_read("padding")
+					}
+					let default = bc_offset + self.read_i32()?;
+					self.explain_read(format!("default branch {:>}", default));
+					let low = self.read_i32()?;
+					self.explain_read("low bound");
+					let high = self.read_i32()?;
+					self.explain_read("high bound");
+					self.indent(1);
+					for key in low..high {
+						let branch = bc_offset + self.read_i32()?;
+						self.explain_read(format!("case {}: branch {:>}", key, branch));
+					}
+					self.indent(-2);
+				}
+				0xc4 => {
+					self.explain_read("wide (JVMS§6.5)");
+					is_wide = true;
+					continue;
+				},
+				_ => break,
+			}
+			if is_wide {
+				self.indent(-1);
+			}
+			is_wide = false;
+		}
+		if self.offset < end {
+			self.read_exact(&mut buf)?;
+			self.explain_read("remaining unknown bytecode");
+		}
+		if self.offset > end {
+			panic!("over read end of bytecode block");
+		}
 		Ok(())
 	}
 }
